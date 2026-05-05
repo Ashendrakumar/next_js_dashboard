1c6e9014030a13d2b7b2d6f311fe98978fff05af
Ashendrakumar <157217684+Ashendrakumar@users.noreply.github.com>
2026-05-05 11:44:26 +0530

    Referece

--- next.config.js
+++ next.config.js
@@ -18,6 +18,22 @@ const nextConfig = {
             key: "X-Frame-Options",
             value: "SAMEORIGIN",
           },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer",
+          },
         ],
       },
     ];
